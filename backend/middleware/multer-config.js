bdbe35307fb081a6831f69c2a908b3fc0f6f6ac0
Mezouar Mohamed <mohmezpro@etik.com>
2024-10-18 15:16:01 +0200

    final final

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,7 +1,9 @@
 const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
+
+sharp.cache(false);
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -16,9 +18,9 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+    const name = file.originalname.split(' ').join('_').split('.')[0];
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, `${name}${Date.now()}.${extension}`);
+    callback(null, `${name}_${Date.now()}.${extension}`);
   }
 });
 
@@ -28,26 +30,31 @@ const compressImage = async (req, res, next) => {
   if (!req.file) {
     return next();
   }
+
   try {
-    const outputPath = path.join('images', `compressed_${req.file.filename}`);
+    const outputPath = path.join('images', `compressed_${Date.now()}.avif`);
 
     await sharp(req.file.path)
       .resize(800)
-      .avif({ quality: 70 })
+      .toFormat('avif', { quality: 70 })
       .toFile(outputPath);
 
-    fs.unlink(req.file.path, (err) => {
-      if (err) {
-        console.error('Erreur lors de la suppression du fichier :', err);
-      } else {
-        console.log('Fichier supprimé avec succès');
-      }
-    });
-          
-    req.file.path = outputPath;
-    req.file.filename = `compressed_${req.file.filename}`;
+    console.log('Conversion terminée, ajout d\'un délai avant suppression...');
 
-    next();
+    setTimeout(async () => {
+      try {
+        await fs.unlink(req.file.path);
+        console.log('Fichier original supprimé avec succès.');
+
+        req.file.path = outputPath;
+        req.file.filename = path.basename(outputPath);
+
+        next();
+      } catch (error) {
+        console.error('Erreur lors de la suppression du fichier :', error);
+        next(error);
+      }
+    }, 500);
   } catch (error) {
     console.error('Erreur lors de la compression de l\'image', error);
     next(error);
